90e2cdafb964a5d5e485d376352bd3ce2f162188
Nabil Bennaï <bennai.nabil@gmail.com>
2026-08-19 20:26:28 +0200

    feat: assign Ligue 1 rounds to synced fixtures

---
+++ src/lib/data/fixtures/ligue1-2026-2027.ts
@@ -0,0 +1,53 @@
+export type Ligue1FixtureDefinition = {
+  round: number
+  homeSlug: string
+  awaySlug: string
+}
+
+export const LIGUE1_2026_2027_FIXTURES: Ligue1FixtureDefinition[] = [
+  {
+    round: 1,
+    homeSlug: "angers-sco",
+    awaySlug: "losc",
+  },
+  {
+    round: 1,
+    homeSlug: "le-havre",
+    awaySlug: "as-monaco",
+  },
+  {
+    round: 1,
+    homeSlug: "le-mans",
+    awaySlug: "stade-brestois",
+  },
+  {
+    round: 1,
+    homeSlug: "rc-lens",
+    awaySlug: "aj-auxerre",
+  },
+  {
+    round: 1,
+    homeSlug: "olympique-marseille",
+    awaySlug: "rc-strasbourg",
+  },
+  {
+    round: 1,
+    homeSlug: "ogc-nice",
+    awaySlug: "fc-lorient",
+  },
+  {
+    round: 1,
+    homeSlug: "paris-sg",
+    awaySlug: "stade-rennais",
+  },
+  {
+    round: 1,
+    homeSlug: "toulouse-fc",
+    awaySlug: "olympique-lyonnais",
+  },
+  {
+    round: 1,
+    homeSlug: "estac-troyes",
+    awaySlug: "paris-fc",
+  },
+]
